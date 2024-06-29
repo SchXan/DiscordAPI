a660311325b2768d739be0db226f79f46907e6f2
SchXan <167179517+SchXan@users.noreply.github.com>
2024-06-29 14:36:58 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 
 const app = express();
 const cors = require('cors');
-const port = process.env.PORT || 8080;
+const port = 4000;
 
 // let allowedOrigins = [
 //   'https://localhost:3000',
